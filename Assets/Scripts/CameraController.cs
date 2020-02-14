593003ed0b27e3a40a67ce42d5d3dfaba94c86c7
Matt Gropper <mgropps17@gmail.com>
2020-02-14 10:50:36 +0100

    camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Camera follows the player along the X axis
+    public Transform player;
+    public float offsetX;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if (player.position.x - transform.position.x < offsetX)
+        {
+            pos.x = player.position.x + offsetX;
+            transform.position = pos;
+        }
+    }
+}

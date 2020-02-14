815ea4afcd3e5443775bf7209fb7957e4079ca13
Matt Gropper <mgropps17@gmail.com>
2020-02-14 11:20:32 +0100

    Platform Spawner and Whitebox Prefab

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public Transform player;
+    public GameObject platform;
+    public int spaceBetweenPlatforms = 25;
+    public int startingXValue = 0;
+    public int offset = 10;
+
+    private GameObject prevPlatform;
+    private int prevX;
+
+    void Start()
+    {
+        prevPlatform = platform;
+        prevX = startingXValue;
+    }
+
+    void Update()
+    {
+        if ((int)player.position.x % spaceBetweenPlatforms == 0 && (int)player.position.x > prevX)
+        {
+            prevX = (int)player.position.x;
+
+            // create new platform
+            Vector3 newPosition = prevPlatform.transform.position;
+            newPosition.x += offset;
+            prevPlatform = Instantiate(platform, newPosition, Quaternion.identity);
+        }
+    }
+
+
+}

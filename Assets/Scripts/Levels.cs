768716cf192f2f211bf86119812ae0fb677a3143
Matt Gropper <mgropps17@gmail.com>
2020-02-18 11:27:06 +0100

    level controller singleton

---
+++ Assets/Scripts/Levels.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levels : MonoBehaviour
+{
+
+    private static Levels first = null;
+
+    private Levels() { }
+
+    private Dictionary<int, Queue<KeyValuePair<char, int>>> songs = new Dictionary<int, Queue<KeyValuePair<char, int>>>();
+
+    void Start()
+    {
+        
+    }
+    
+
+    public static Levels getInstance()
+    {
+        if (first == null)
+        {
+            first = new Levels();
+        }
+
+        return first;
+    }
+
+    public KeyValuePair<char, int> getNextNote(int levelNum)
+    {
+
+        return songs[levelNum].Dequeue();
+    }
+}

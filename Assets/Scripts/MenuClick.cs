cbb41606f781f9c023a823253481600bbb28804b
Josh Londin <jgl92@cornell.edu>
2020-03-03 09:08:51 +0100

    implemented start menu

---
+++ Assets/Scripts/MenuClick.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuClick : MonoBehaviour
+{
+    public int level;
+    TextMesh tm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        tm.color =Color.red;
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Level"+level);
+    }
+
+    private void OnMouseExit()
+    {
+        tm.color = Color.white;
+    }
+}

4bcfdaa797de671b096898f2142f5d2094719aab
Ethan Ruoff <ethanruoff@gmail.com>
2020-03-06 17:02:44 +0100

    Swan Lake

--- Assets/Scripts/Levels.cs
+++ Assets/Scripts/Levels.cs
@@ -68,7 +68,40 @@ public class Levels : MonoBehaviour
             first.songs.Add(1, level1);
 
             Queue<KeyValuePair<char, int>> level2 = new Queue<KeyValuePair<char, int>>();
-            level2.Enqueue(new KeyValuePair<char, int>('b', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('e', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('b', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('d', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('e', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('e', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('f', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 4));
+            //2nd half
+            level2.Enqueue(new KeyValuePair<char, int>('d', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('b', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('b', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('d', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('e', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('e', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('f', 4));
+            level2.Enqueue(new KeyValuePair<char, int>('c', 5));
+            level2.Enqueue(new KeyValuePair<char, int>('a', 4));
+
+            first.songs.Add(2, level2);
         }
 
         return ref first;

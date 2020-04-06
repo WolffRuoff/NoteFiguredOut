3711219cb6341198dd63bf3ee0e62882de02f264
Matt Gropper <mgropps17@gmail.com>
2020-04-06 17:51:06 -0400

    tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,183 +1,296 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    public GameObject player;
-    public float yValueIncrease;
-    public float xValueIncrease;
-    public Button[] buttons;
-    public float timerVal = 2f;
-    public Text[] text;
-    public int indexer;
-    public bool random = false;
-    public int level = 0;
-    public GameObject IGUI;
-    public GameObject YWUI;
-    public CanvasGroup cg;
-    public Text points;
-    public CanvasGroup igcg;
+    public GameObject[] texts = new GameObject[7];
+    public GameObject leftArrow;
+    public GameObject Peter;
+    public GameObject buttons;
+    public Button c;
+    public Button d;
+    public Button e;
+    public Button f;
+    public Button g;
+    public Button a;
+    public Button b;
+    public EventSystem es;
+    public GameObject timeBar;
+    public GameObject time;
+    public GameObject scoreT;
+    public GameObject pause;
 
-    private KeyValuePair<char, int> pair;
-    private static bool active = true;
-    
+    private int pos = 0;
+    private float timer = 4f;
+    private float maxTime = 4f;
+    private bool blink = false;
+    private float score = 0.0f;
+    private char selection = '\0';
 
-    private static int note = -1;
-    private static int selection = -1;
-    private float timer;
-
-    // Start is called before the first frame update
     void Start()
     {
-        indexer = 0;
-        text[0].enabled = true;
-        igcg = IGUI.GetComponent<CanvasGroup>();
-        active = false;
-
+        pos = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.Return) && pos == 1)
         {
-            if (indexer == 0)
-            {
-                indexer++;
-                text[0].enabled = false;
-                text[indexer].enabled = true;
-            }
+            char note;
+            int randNote = UnityEngine.Random.Range(1, 7);
 
-            if (indexer == 1)
+            if (randNote == 1)
             {
-                indexer++;
-                text[1].enabled = false;
-                text[indexer].enabled = true;
+                note = 'c';
             }
-
-            if (indexer == 2)
+            else if (randNote == 2)
             {
-                indexer++;
-                text[2].enabled = false;
-                text[indexer].enabled = true;
+                note = 'd';
             }
-
-            if (indexer == 3)
+            else if (randNote == 3)
             {
-                text[3].enabled = false;
-                SceneManager.LoadScene("StartMenu");
-                //End the tutorial here
+                note = 'e';
             }
-
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            else if (randNote == 4)
+            {
+                note = 'f';
+            }
+            else if (randNote == 5)
+            {
+                note = 'g';
+            }
+            else if (randNote == 6)
+            {
+                note = 'a';
+            }
+            else
+            {
+                note = 'b';
+            }
+            Notes.Instance.Note(note, 4);
+        } else if (pos == 3 || pos == 4 || pos == 5)
         {
-
-            if (indexer == 1)
+            if (Input.GetKeyDown(KeyCode.C))
             {
-                indexer--;
-                text[1].enabled = false;
-                text[indexer].enabled = true;
-            }
-
-            if (indexer == 2)
-            {
-                indexer--;
-                text[2].enabled = false;
-                text[indexer].enabled = true;
-            }
-
-            if (indexer == 3)
-            {
-                indexer--;
-                text[3].enabled = false;
-                text[indexer].enabled = true;
-            }
-        }
-
-        if (indexer == 2)
-        {
-            active = true;  
-        }
-
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("player"))
-        {
-            // stop player from sliding and recenter so that it never goes off the edge
-            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-            GameController.center();
-
-            if (active)
-            {
-                // send info to GameController
-                GameController.RecievePlatform(transform);
-
-                // assign platform a note
-
-                Levels curr = Levels.getInstance();
-                if (!curr.isEmpty(level))
+                c.Select();
+                if (selection != 'c')
                 {
-                    pair = curr.getNextNote(level);
+                    score = timer;
+                }
+                selection = 'c';
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                d.Select();
+                if (selection != 'd')
+                {
+                    score = timer;
+                }
+                selection = 'd';
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                e.Select();
+                if (selection != 'e')
+                {
+                    score = timer;
+                }
+                selection = 'e';
+            }
+            else if (Input.GetKeyDown(KeyCode.F))
+            {
+                f.Select();
+                if (selection != 'f')
+                {
+                    score = timer;
+                }
+                selection = 'f';
+            }
+            else if (Input.GetKeyDown(KeyCode.G))
+            {
+                g.Select();
+                if (selection != 'g')
+                {
+                    score = timer;
+                }
+                selection = 'g';
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                a.Select();
+                if (selection != 'a')
+                {
+                    score = timer;
+                }
+                selection = 'a';
+            }
+            else if (Input.GetKeyDown(KeyCode.B))
+            {
+                b.Select();
+                if (selection != 'b')
+                {
+                    score = timer;
+                }
+                selection = 'b';
+            }
+        }
+
+        if (pos == 4 || pos == 5)
+        {
+            if (updateTimer())
+            {
+                scoreT.GetComponent<Text>().text = "Score: " + Math.Round(score, 2);
+                timer = maxTime;
+                timeBar.GetComponent<Image>().color = Color.white;
+                es.SetSelectedGameObject(null);
+                selection = '\0';
+            }
+        }
+    }
+
+    public void updateText (int leftRight)
+    {
+        es.SetSelectedGameObject(null);
+
+        if (leftRight == 1)
+        {
+            // right arrow key hit
+            if (pos == 0)
+            {
+                texts[pos++].SetActive(false);
+                texts[pos].SetActive(true);
+                Peter.SetActive(false);
+                leftArrow.SetActive(true);
+                Notes.Instance.setActive(true);
+            }
+            else if (pos == 1)
+            {
+                texts[pos++].SetActive(false);
+                texts[pos].SetActive(true);
+                buttons.SetActive(true);
+                Notes.Instance.setActive(false);
+            }
+            else if (pos == 2)
+            {
+                texts[pos++].SetActive(false);
+                texts[pos].SetActive(true);
+            }
+            else if (pos == 3)
+            {
+                texts[pos++].SetActive(false);
+                texts[pos].SetActive(true);
+                time.SetActive(true);
+                timeBar.SetActive(true);
+                scoreT.SetActive(true);
+
+}
+            else if (pos == 4)
+            {
+                texts[pos++].SetActive(false);
+                texts[pos].SetActive(true);
+                pause.SetActive(true);
+            }
+            else if (pos == 5)
+            {
+                texts[pos++].SetActive(false);
+                texts[pos].SetActive(true);
+                buttons.SetActive(false);
+                timeBar.SetActive(false);
+                time.SetActive(false);
+                scoreT.SetActive(false);
+                pause.SetActive(false);
+            }
+            else if (pos == 6)
+            {
+                SceneManager.LoadScene("TutorialLevel");
+            }
+            
+        } else
+        {
+            if (pos == 1)
+            {
+                texts[pos--].SetActive(false);
+                texts[pos].SetActive(true);
+                Peter.SetActive(true);
+                leftArrow.SetActive(false);
+                Notes.Instance.setActive(false);
+            }
+            else if (pos == 2)
+            {
+                texts[pos--].SetActive(false);
+                texts[pos].SetActive(true);
+                buttons.SetActive(false);
+                Notes.Instance.setActive(true);
+            }
+            else if (pos == 3)
+            {
+                texts[pos--].SetActive(false);
+                texts[pos].SetActive(true);
+            }
+            else if (pos == 4)
+            {
+                texts[pos--].SetActive(false);
+                texts[pos].SetActive(true);
+                time.SetActive(false);
+                timeBar.SetActive(false);
+                scoreT.SetActive(false);
+            }
+            else if (pos == 5)
+            {
+                texts[pos--].SetActive(false);
+                texts[pos].SetActive(true);
+                pause.SetActive(false);
+            }
+            else if (pos == 6)
+            {
+                texts[pos--].SetActive(false);
+                texts[pos].SetActive(true);
+                buttons.SetActive(true);
+                time.SetActive(true);
+                timeBar.SetActive(true);
+                scoreT.SetActive(true);
+                pause.SetActive(true);
+            }
+        }
+    }
+
+    public void hitButton (string selec)
+    {
+        if (selec[0] != selection)
+        {
+            score = timer;
+            selection = selec[0];
+        }
+        
+    }
+
+    private bool updateTimer()
+    {
+        timer -= Time.deltaTime;
+        timeBar.GetComponent<Image>().fillAmount = timer / maxTime;
+
+        if (timer > 0)
+        {
+            if (timer < maxTime / 3)
+            {
+                if (blink == false)
+                {
+                    timeBar.GetComponent<Image>().color = Color.red;
+                    blink = true;
                 }
                 else
                 {
-                    // player wins tutorial
-                    indexer = 3;
-                    text[2].enabled = false;
-                    text[3].enabled = true;
-
-
-                    Levels.setFirst();
-                    GameController.setActive(false);
-                    GameController.setWon(true);
-                    Notes.Instance.setActive(false);
-                    InputController.setActive(false);
-                    CameraController.setActive(false);
-                       
-                    active = false;
+                    timeBar.GetComponent<Image>().color = Color.white;
+                    blink = false;
                 }
-
-
-                // send selection to GameController
-                /*if (pair.Key == 'c')
-                {
-                    GameController.RecieveNote(0);
-                }
-                else if (pair.Key == 'd')
-                {
-                    GameController.RecieveNote(1);
-                }
-                else if (pair.Key == 'e')
-                {
-                    GameController.RecieveNote(2);
-                }
-                else if (pair.Key == 'f')
-                {
-                    GameController.RecieveNote(3);
-                }
-                else if (pair.Key == 'g')
-                {
-                    GameController.RecieveNote(4);
-                }
-                else if (pair.Key == 'a')
-                {
-                    GameController.RecieveNote(5);
-                }
-                else if (pair.Key == 'b')
-                {
-                    GameController.RecieveNote(6);
-                }*/
-
-                // play the note
-                Notes.Instance.Note(pair.Key, pair.Value);
-                Debug.Log(pair.Key);
             }
+            return false;
         }
+        return true;
     }
 }
